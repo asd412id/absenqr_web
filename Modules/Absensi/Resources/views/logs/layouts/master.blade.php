86356220cca10c9c21d2f0d6c7a517f08e463193
asd412id <asdar.binsyam@gmail.com>
2020-01-20 21:10:45 +0800

    Change filter jadwal title in absensi log

--- Modules/Absensi/Resources/views/logs/layouts/master.blade.php
+++ Modules/Absensi/Resources/views/logs/layouts/master.blade.php
@@ -112,7 +112,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="daftarjadwal-title">Daftar Jadwal</h5>
+          <h5 class="modal-title" id="daftarjadwal-title">Filter Jadwal</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
