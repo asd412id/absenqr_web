416445c89f84d84bfcbc916847ef2ed1774f1886
asd412id <asdar.binsyam@gmail.com>
2023-05-15 14:50:55 +0800

    Fix: TTD Format

--- resources/views/layouts/ttd.blade.php
+++ resources/views/layouts/ttd.blade.php
@@ -4,8 +4,9 @@
 <table style="width: 100%">
   <tr>
     <td style="width: 60%;white-space: nowrap">
-      @if (strpos(Request::url(),route('absensi.log.index'))!==false)
-        <table class="" style="margin-top: 25px;float: left;page-break-inside: avoid !important;white-space: nowrap;">
+      @if (strpos(Request::url(), route('absensi.log.index')) !== false)
+        <table class=""
+          style="margin-top: 25px;float: left;page-break-inside: avoid !important;white-space: nowrap;">
           <tr>
             <td class="font-weight-bold" colspan="2">Keterangan Warna Absen:</td>
           </tr>
@@ -14,54 +15,60 @@
             <td class="status-color" style="padding-left: 10px">Kehadiran Tepat Waktu</td>
           </tr>
           <tr>
-             <td class="status-color bg-warning" style="display: inline-block;margin: 1px;border: solid 1px #000"></td>
+            <td class="status-color bg-warning" style="display: inline-block;margin: 1px;border: solid 1px #000"></td>
             <td style="padding-left: 10px">Terlambat / Pulang Cepat</td>
           </tr>
           <tr>
-             <td class="status-color bg-success" style="display: inline-block;margin: 1px;border: solid 1px #000"></td>
+            <td class="status-color bg-success" style="display: inline-block;margin: 1px;border: solid 1px #000"></td>
             <td style="padding-left: 10px">Sakit / Izin / Keterangan</td>
           </tr>
           <tr>
-             <td class="status-color bg-danger" style="display: inline-block;margin: 1px;border: solid 1px #000"></td>
+            <td class="status-color bg-danger" style="display: inline-block;margin: 1px;border: solid 1px #000"></td>
             <td style="padding-left: 10px">Tidak Hadir</td>
           </tr>
         </table>
-      @elseif (strpos(Request::url(),route('absensi.log.rekap'))!==false)
+      @elseif (strpos(Request::url(), route('absensi.log.rekap')) !== false)
         <table style="width: 60%;margin-top: 25px;float: left;page-break-inside: avoid !important;white-space: nowrap;">
           <tr>
             <td style="font-weight: bold;padding-left: 0" colspan="2">Keterangan Absen:</td>
           </tr>
           <tr>
-            <td class="status-color" style="display: inline-block;text-align: center;margin: 1px;border: solid 1px #000">
+            <td class="status-color"
+              style="display: inline-block;text-align: center;margin: 1px;border: solid 1px #000">
               &#10004;
             </td>
             <td style="padding-left: 10px">Hadir</td>
           </tr>
           <tr>
-            <td class="status-color bg-warning" style="display: inline-block;text-align: center;margin: 1px;border: solid 1px #000"></td>
+            <td class="status-color bg-warning"
+              style="display: inline-block;text-align: center;margin: 1px;border: solid 1px #000"></td>
             <td style="padding-left: 10px">Sakit / Izin / Cuti</td>
           </tr>
         </table>
       @endif
     </td>
     <td style="text-align: right;white-space: nowrap !important">
-      <table style="margin-top: 25px;float: right !important;page-break-inside: avoid !important;white-space: nowrap;text-align: left">
+      <table
+        style="margin-top: 25px;float: right !important;page-break-inside: avoid !important;white-space: nowrap;text-align: left">
         <tr>
-          <td style="height: 30px;">{{ @$config->kota??'Sinjai' }}, {{ \Carbon\Carbon::parse(@request()->end_date)->locale('id')->translatedFormat('d F Y') }}</td>
+          <td style="height: 30px;">{{ @$config->kota ?? 'Sinjai' }},
+            {{ \Carbon\Carbon::parse(@request()->end_date)->locale('id')->translatedFormat('d F Y') }}</td>
         </tr>
         <tr>
           <td>Mengetahui,</td>
         </tr>
         <tr>
-          <td>Kepala {{ @$configs->nama_instansi??'UPTD SMP Negeri 39 Sinjai' }}</td>
+          <td>Kepala {{ @$configs->kop ? 'Sekolah' : @$configs->nama_instansi ?? 'UPTD SMP Negeri 39 Sinjai' }}</td>
         </tr>
         <tr>
           <td style="height: 75px"></td>
         </tr>
         <tr>
           <td style="line-height: 1em;font-weight: bold">
-            {!! @$configs->pimpinan?nl2br($configs->pimpinan):'SITTI SAIDAH SUYUTI, S.Pd.,M.Pd.<br>
-            NIP. 19710626 199702 2 005' !!}
+            {!! @$configs->pimpinan
+                ? nl2br($configs->pimpinan)
+                : 'SITTI SAIDAH SUYUTI, S.Pd.,M.Pd.<br>
+                        NIP. 19710626 199702 2 005' !!}
           </td>
         </tr>
       </table>
